f1bfb7504395f814819a6431faddda63a2e2fa69
Stefan Mansson <stefan.aotik@gmail.com>
2019-01-10 20:57:40 +0200

    Add download icon to Designer Pack

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import logotype from '../assets/logotype.svg';
 import * as pkg from '../../package.json';
+import Figicon from '../../Figicons/components/FigiconReact';
 
 const Nav = () => (
     <nav>
@@ -14,7 +15,10 @@ const Nav = () => (
                 <Link to={'/building-icons'}>Building custom icons</Link>
                 <Link to={'/usage'}>Usage</Link>
                 <Link to={'/faq'}>FAQ</Link>
-                <div className="button small grey">Designer Pack</div>
+                <div className="button small grey">
+                    <Figicon name="download" width={14} height={14} style={{ marginRight: '6px' }} />
+                    Designer Pack
+                </div>
             </div>
         </div>
     </nav>
